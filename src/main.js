ccd81e4a8da7e4ef6c3bfcdc24c18f25983473b7
Sahil <sahil5082.k@gmail.com>
2024-11-06 19:16:07 -0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/hero.css';
+import '../styles/components/about.css';
+import '../styles/components/projects.css';
+import '../styles/components/certificates.css';
+import '../styles/components/contact.css';
+import '../styles/components/footer.css';
+import '../styles/components/mobile-nav.css';
+import '../styles/util.css';
+
+import mobileNav from './mobile-nav';
+mobileNav();
